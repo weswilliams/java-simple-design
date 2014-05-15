c2b4c075b6fbf5191b5ad3d4b92f9c54c1c3d5ce
weswilliams <wes.williamz@gmail.com>
2014-05-15 11:09:40 +0800

    remove unused files, added starting test for tdd intro, fixed .gitignore, updated junit dependency

---
+++ src/test/java/simpledesign/section1TddIntro/CellTest.java
@@ -0,0 +1,13 @@
+package simpledesign.section1TddIntro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+// See http://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
+public class CellTest {
+	@Test
+	public void deadCellShouldStayDeadWithNoLiveNeighbors() {
+		assertTrue("should be working", true);
+	}
+}
